cf7d9903cf0517755907eb96a9fe2f2e9cbb4468
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-08-05 23:21:11 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,6 +21,6 @@ class Doctor
   end
 
   def patients
-    appointments.collect {|appointment| appointment.patient == self}
+    appointments.collect {|appointment| appointment.patient}
   end
 end
